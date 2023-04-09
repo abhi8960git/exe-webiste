2ac3b17ce144175248578beac0040c525ba1c398
Piyush@laptop <chandra7piyush@gmail.com>
2023-04-09 20:26:58 +0530

    small c

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import "@/styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
-import Particle from "@/Components/Particle";
-import Navbar from "@/Components/Navbar/Navbar";
+import Particle from "@/components/Particle";
+import Navbar from "@/components/Navbar/Navbar";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
     return (
         <>
             <Particle />
-          
+
             <Component {...pageProps} />
 
             <Navbar />
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-import Footer from "@/Components/Footer/Footer";
+import Footer from "@/components/Footer/Footer";
 import classes from "@/styles/Home.module.css";
-import Main from "@/Components/Home/Home";
-import Social from "@/Components/SocialIcon/Social";
+import Main from "@/components/Home/Home";
+import Social from "@/components/SocialIcon/Social";
 
 const Home = () => {
     return (
         <>
+            <div className={classes.indexContainer}>
+                <div className={classes.header}>
+                    <h1>TEAM .EXE</h1>
+                    <p>Executing Ideas</p>
+                </div>
+                <Main />
 
-        <div className={classes.indexContainer}>
-            <div className={classes.header}>
-                <h1>TEAM .EXE</h1>
-                <p>Executing Ideas</p>
-            </div>
-            <Main />
-
-            <div className={classes.footer}>
-                <Footer />
-            </div>
-
+                <div className={classes.footer}>
+                    <Footer />
+                </div>
             </div>
         </>
     );
