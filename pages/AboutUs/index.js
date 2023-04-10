1be710b9fdf2081a51c83af823eacebafafe0c20
Abhishek <21bcs116@nith.ac.in>
2023-04-09 19:03:24 -0700

    about_us

--- pages/AboutUs/index.js
+++ pages/AboutUs/index.js
@@ -1,70 +1,51 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import { EffectCoverflow, Pagination, Navigation } from "swiper";
-import classes from "./AboutUs.module.css";
-import { Card } from "@/components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-const AboutUs = () => {
-    // const [dimension, setDimension] = useState([
-    //     window.innerHeight,
-    //     window.innerWidth,
-    // ]);
-    // console.log(innerHeight);
-    return (
-        <div>
-            <h1 className={classes.head}>About Us</h1>
-            <Swiper
-                effect={"coverflows"}
-                grabCursor={true}
-                centeredSlides={true}
-                loop={true}
-                slidesPerView={3}
-                coverflowEffect={{
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 2.5,
-                }}
-                modules={[EffectCoverflow, Pagination, Navigation]}
-                className={classes.swiper}
-            >
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <div className={classes.slider_controler}>
-                    <div
-                        className={`${classes.swiper_button_prev} ${classes.slider_arrow}`}
-                    >
-                        <FontAwesomeIcon icon={faArrowLeft} />
-                    </div>
-                    <div
-                        className={`${classes.swiper_button_next} ${classes.slider_arrow}`}
-                    >
-                        <FontAwesomeIcon icon={faArrowRight} />
-                    </div>
-                    <div className={classes.swiper_pagination}></div>
-                </div>
-            </Swiper>
-        </div>
-    );
-};
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import { Card } from "../../components/index";
+import css from "./AboutUs.module.css";
 
-export default AboutUs;
+
+
+const index = () => {
+    const [width, setWidth] = useState(0);
+    const carousel = useRef();
+
+    useEffect(()=>{
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth );
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth );
+    },[])
+
+    return (
+        <div className="App">
+
+           
+
+            <h1 className={css.head}>About Us</h1>
+
+
+            <motion.div ref={carousel} className={css.carousel}>
+                <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className={css.innerCarousel}>
+
+                    <motion.div className="item">
+                        <Card></Card>
+                    </motion.div>
+
+                    <motion.div className="item">
+                        <Card></Card>
+                    </motion.div>
+
+                    <motion.div className="item">
+                        <Card></Card>
+                    </motion.div>
+
+                    <motion.div className="item">
+                        <Card></Card>
+                    </motion.div>
+
+                </motion.div>
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default index
