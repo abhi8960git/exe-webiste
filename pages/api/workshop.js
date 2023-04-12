e8a6d01b4e5abe8d1248afe3b1aa6bdb716757e4
Abhishek <21bcs116@nith.ac.in>
2023-04-12 07:01:01 -0700

    changes

--- pages/api/workshop.js
+++ pages/api/workshop.js
@@ -12,7 +12,7 @@ const workshop = [
   { 
     id:1,
     image:"https://res.cloudinary.com/dcln4oee5/image/upload/v1681233292/bug-hunting_workshop.jpg",
-    title: "Hacking Billion Doller Companies-A BUG HUNTING WORKSHOP",
+    title: "Hacking Billion Dollar Companies-A BUG HUNTING WORKSHOP",
     detail: "Through this workshop, participants acquired the fundamentals of security vulnerabilities, with a focus on web-specific flaws, and got hands-on practice detecting bugs on real platforms.The information-gathering phase of ethical hacking, which can span anything from network infrastructure to employee contact information, was explained to the participants. Reconnaissance was to locate as many potential assault vectors as was practical.The program covered systematically examining security flaws in an information system, determining whether it is susceptible to any known vulnerabilities, classifying those vulnerabilities according to severity, and recommending corrections if needed.Participants learned how to use an application's static and dynamic properties to identify malware and benign applications.The fundamentals of smart contract security were described.In order for their findings to be correctly addressed, information was provided on how to submit the bugs with the correct documentation and proof of concept.",
     mentors: "Anurag Verma , Shubham Jaswal"
   }
